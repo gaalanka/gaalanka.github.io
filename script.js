39a6d92ffffbaf37b89bcc563871db652bb053f1
Gaál Anna <gaalankaa@gmail.com>
2024-10-03 22:07:40 +0200

    js hozzáadása (randomuser.me)

---
+++ script.js
@@ -0,0 +1,11 @@
+// Fetch data from Random User API
+fetch('https://randomuser.me/api/')
+  .then(response => response.json())
+  .then(data => {
+    const user = data.results[0];
+    document.getElementById('user-photo').src = user.picture.large;
+    document.getElementById('user-name').textContent = `${user.name.first} ${user.name.last}`;
+    document.getElementById('user-email').textContent = user.email;
+    document.getElementById('user-location').textContent = `${user.location.city}, ${user.location.country}`;
+  })
+  .catch(error => console.error('Error fetching user data:', error));
